f06dc0e1dd1e692e91356a976079a09a7d8504a1
ana beatriz dani bernardo <118557668+anabdani@users.noreply.github.com>
2023-10-09 12:17:36 -0300

    Create aula8.py

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Ana Beatriz'
+sobrenome = 'Dani'
+idade = '20'
+
+
+altura_metros = 1.80
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade', idade)
+print('Ano de nascimento:', ano_de_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros', altura_metros)
