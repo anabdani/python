7fa7f82510642db75dd44d58f314fcc67849d474
ana beatriz dani bernardo <118557668+anabdani@users.noreply.github.com>
2023-10-23 13:35:37 -0300

    aulas

---
+++ aula23.py
@@ -0,0 +1,7 @@
+# Operador lógico "não"
+# Usado para inverter expressões
+# não é verdadeiro = falso
+# não falso = verdadeiro
+# senha = input('Senha: ')
+imprimir ( não  verdadeiro )   #Falso
+imprimir ( não  falso )   # Verdadeiro
