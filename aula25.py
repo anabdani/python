b72a5dde68ff9bd4691e029ea05b27aa8e5521e8
ana beatriz dani bernardo <118557668+anabdani@users.noreply.github.com>
2023-10-25 13:58:06 -0300

    exercicio

---
+++ aula25.py
@@ -0,0 +1,12 @@
+"""
+Interpolação básica de strings
+s - corda
+dei-int
+f - flutuar
+xeX - Hexadecimal (ABCDEF0123456789)
+"""
+nome  =  'Luiz'
+preço  =  1000,95897643
+variavel  =  '%s, o preço é R$%.2f'  % ( nome , preço )
+print ( variável )
+print ( 'O hexadecimal de %d é %08X'  % ( 1500 , 1500 ))
