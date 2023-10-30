cfde9660b92b285e817b111425edc9239c6db17e
ana beatriz dani bernardo <118557668+anabdani@users.noreply.github.com>
2023-10-30 12:59:33 -0300

    exercícios

---
+++ aula31.py
@@ -0,0 +1,20 @@
+"""
+Flag (Bandeira) - Marcar um local
+Nenhum = Não tem valor
+é e não é = é ou não é (tipo, valor, identidade)
+id = Identidade
+"""
+condição  =  False
+passou_no_if  =  None
+
+if  condicao :
+    passou_no_if  =  True
+    print ( 'Faça algo' )
+else :
+    print ( 'Não faça algo' )
+
+
+if  passou_no_if  is None :
+    print ( 'Não passou no if' )
+else :
+    print ( 'Passou no if' )
